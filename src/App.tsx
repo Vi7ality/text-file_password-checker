67e406400a6298e99fa861aadb49337a0cb9b6b3
Vitalii S. <svitalii138@gmail.com>
2024-09-17 15:15:55 +0300

    Password validator component created

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,11 @@
+import PasswordValidator from 'components/PasswordValidator';
 import './App.css';
 
 function App() {
   return (
     <>
       <h1>Password validator</h1>
+      <PasswordValidator />
     </>
   );
 }
---
+++ src/components/PasswordValidator/index.tsx
@@ -0,0 +1,10 @@
+const PasswordValidator = () => {
+  return (
+    <div>
+      <h1>Перевірка валідності паролів</h1>
+      <input type="file" accept=".txt" />
+    </div>
+  );
+};
+
+export default PasswordValidator;
